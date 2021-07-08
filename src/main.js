d586ce612bc638fa651448ee688ca46cfd1e61a6
MVictoriaBezares <mariavictoriabezares@gmail.com>
2021-07-08 10:45:37 -0400

    mi portafolio aun por actualizar

---
+++ src/main.js
@@ -0,0 +1,41 @@
+// //Funcionalidad botones
+// ////Boton 1 Herramientas/////
+// let buttonTools = document.getElementById('tools');
+// buttonTools.addEventListener('click', () => { 
+//   document.getElementById('pagTwo').style.display = 'block';
+//   document.getElementById('pagOne').style.display = 'none';
+//   document.getElementById('pagTre').style.display = 'none';
+//   document.getElementById('pagFour').style.display = 'none';
+//   document.getElementById('pagFive').style.display = 'none';
+// });
+  
+// ////Boton 2 Proyectos/////
+// let buttonProjects = document.getElementById('projects');
+// buttonProjects.addEventListener('click', () => { 
+//   document.getElementById('pagTre').style.display = 'block';
+//   document.getElementById('pagOne').style.display = 'none';
+//   document.getElementById('pagTwo').style.display = 'none';
+//   document.getElementById('pagFour').style.display = 'none';
+//   document.getElementById('pagFive').style.display = 'none';
+// });
+
+// ////Boton 3 Contacto/////
+// let buttonContact = document.getElementById('contact');
+// buttonContact.addEventListener('click', () => { 
+//   document.getElementById('pagFour').style.display = 'block';
+//   document.getElementById('pagFive').style.display = 'block';
+//   document.getElementById('pagOne').style.display = 'none';
+//   document.getElementById('pagTwo').style.display = 'none';
+//   document.getElementById('pagTre').style.display = 'none';
+// });
+
+// ////Boton 4  Ir al Inicio////
+// let buttonToGo = document.getElementById('to-go');
+// buttonToGo.addEventListener('click', () => { 
+//   document.getElementById('pagOne').style.display = 'block';
+//   document.getElementById('pagTwo').style.display = 'none';
+//   document.getElementById('pagTre').style.display = 'none';
+//   document.getElementById('pagFour').style.display = 'none';
+//   document.getElementById('pagFive').style.display = 'none';
+// });
+
